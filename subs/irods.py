9b77f099548e3ce60fbfe37a67cbd20eb52e744c
Tammo Jan Dijkema <dijkema@astron.nl>
2018-10-15 10:45:04 +0200

    Use new alta dir also in check_alta

--- subs/irods.py
+++ subs/irods.py
@@ -1,21 +1,14 @@
 import os
-from getdata_alta import getdata_alta
+from getdata_alta import getdata_alta, get_alta_dir
 
-def getstatus_alta(date, ID, beam):
+def getstatus_alta(date, task_id, beam):
     '''
     Funtion to check if the data is on ALTA.
-    date (int): Date of the observing weekend of the data. Usually the first day of the weekend (Friday). Format: YYMMDD
-    ID (int): ID number of the observation. Format: NNN
-    beam (int): Beam number to copy. Format: NN
+    date (int or str): Date of the observing weekend of the data. Usually the first day of the weekend (Friday). Format: YYMMDD
+    task_id (int or str): ID number of the observation. Format: NNN
+    beam (int or str): Beam number to copy. Format: NN
     return (bool): True if the file is available, False if not
     '''
-    if int(date) <= 180216:
-        cmd = "ils /altaZone/home/apertif_main/wcudata/WSRTA%s%.2d/WSRTA%s%.2d_B%.3d.MS " % (int(date), int(ID), int(date), int(ID), int(beam))
-    else:
-        cmd = "ils /altaZone/home/apertif_main/wcudata/WSRTA%s%.3d/WSRTA%s%.3d_B%.3d.MS " % (int(date), int(ID), int(date), int(ID), int(beam))
-    errorcode = os.system(cmd)
-    if errorcode == 0:
-        status = True
-    else:
-        status = False
-    return status
+    altadir = get_alta_dir(date, int(task_id), int(beam), False)
+    cmd = "ils {} ".format(altadir)
+    return os.system(cmd) == 0
