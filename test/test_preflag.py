0c0dc20a97f9caa98d88bfd7d1b8ae59b9a3611d
Gijs Molenaar <gijs@pythonic.nl>
2018-11-02 13:42:56 +0100

    add no dirification teset

--- test/test_preflag.py
+++ test/test_preflag.py
@@ -9,14 +9,24 @@ import logging
 logging.basicConfig(level=logging.INFO)
 here = path.dirname(__file__)
 
+data_prefix = path.join(here, '../data/small/00/raw/')
+
 
 class TestPreflag(unittest.TestCase):
     def test_preflag(self):
-        p = preflag(path.join(here, 'test.cfg'))
+        p = preflag()
         p.basedir = path.join(here, '../data/small/')
         p.fluxcal = '3C295.MS'
         p.polcal = '3C138.MS'
         p.target = 'NGC807.MS'
+        p.go()
+
+    def test_preflag_nosubdirs(self):
+        p = preflag()
+        p.subdirification = False
+        p.fluxcal = path.join(data_prefix, '3C295.MS')
+        p.polcal = path.join(data_prefix, '3C138.MS')
+        p.target = path.join(data_prefix, 'NGC807.MS')
         p.go()
 
 
