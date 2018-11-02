a314b6ae5c812fdc4161d8a6df2d95e593a45740
Gijs Molenaar <gijs@pythonic.nl>
2018-11-02 10:16:54 +0100

    we dont need apercal anymore

--- test/test_prepare.py
+++ test/test_prepare.py
@@ -16,13 +16,12 @@ class TestPrepare(unittest.TestCase):
 
     def test_prepare(self):
         p = prepare(path.join(here, 'test.cfg'))
-        p.apercaldir = path.join(here, '../')
         p.basedir = path.join(here, '../data/small/')
         p.fluxcal = '3C295.MS'
         p.polcal = '3C138.MS'
         p.target = 'NGC807.MS'
-        #p.show(showall=False)
         p.go()
+
 
 if __name__ == "__main__":
         unittest.main()
--- test/test_transfer.py
+++ test/test_transfer.py
@@ -12,12 +12,11 @@ class TestTransfer(unittest.TestCase):
 
     def test_transfer(self):
         p = transfer(path.join(here, 'test.cfg'))
-        p.apercaldir = path.join(here, '../')
         p.basedir = path.join(here, '../data/small/')
         p.fluxcal = '3C295.MS'
         p.polcal = '3C138.MS'
         p.target = 'NGC807.MS'
-        #p.show(showall=False)
+
 
 if __name__ == "__main__":
         unittest.main()
