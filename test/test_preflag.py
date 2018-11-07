0b6be09ba9e1799ed82b3c082958d314b80da72c
Tammo Jan Dijkema <dijkema@astron.nl>
2018-11-07 22:49:43 +0100

    Fix travis with larger test set

--- test/test_preflag.py
+++ test/test_preflag.py
@@ -19,7 +19,7 @@ class TestPreflag(unittest.TestCase):
         p.polcal = '3C138.MS'
         p.target = 'NGC807.MS'
         p.prepare_bypass_alta = True
-        p.prepare_target_beams = '00, 04, 17'
+        p.prepare_target_beams = '00,04'
         p.go()
 
     def test_preflag_nosubdirs(self):
@@ -29,7 +29,7 @@ class TestPreflag(unittest.TestCase):
         p.polcal = path.join(data_prefix, '3C138.MS')
         p.target = path.join(data_prefix, 'NGC807.MS')
         p.prepare_bypass_alta = True
-        p.prepare_target_beams = '00, 04, 17'
+        p.prepare_target_beams = '00,04'
         p.go()
 
 
