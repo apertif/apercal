c1782c78af6ca6d0018695bc9acb9e51130ae4fb
Tammo Jan Dijkema <dijkema@astron.nl>
2018-12-06 09:23:21 +0100

    Test pep8, don't merge this commit

--- test/test_preflag.py
+++ test/test_preflag.py
@@ -15,6 +15,8 @@ class TestPreflag(unittest.TestCase):
     def test_preflag(self):
         p = preflag()
         p.basedir = path.join(here, '../data/small/')
+        p.fluxcal = "This is just a very long line to see if pep8speaks still ignores .pep8speaks.yml"
+        p.fluxcal = "This is a ridiculously long line on which pep8speaks should trigger, because the line is longer than 120 characters"
         p.fluxcal = '3C295.MS'
         p.polcal = '3C138.MS'
         p.target = 'NGC807.MS'
