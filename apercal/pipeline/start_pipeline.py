85871a029e47148a1d567a5bb217693a6d20da74
Tammo Jan Dijkema <dijkema@astron.nl>
2018-12-14 17:00:26 +0100

    Disable convert
    Two reasons:
    - it fails for too many channels
    - in new selfcal it will come after scal step

--- apercal/pipeline/start_pipeline.py
+++ apercal/pipeline/start_pipeline.py
@@ -88,7 +88,7 @@ def start_apercal_pipeline((taskid_fluxcal, name_fluxcal, beamlist_fluxcal),
 
         p3 = convert()
         set_files(p3)
-        p3.go()
+        #p3.go()
 
         time_end = time()
         msg = "Apercal finished after " + timedelta(seconds=time()-time_start)
