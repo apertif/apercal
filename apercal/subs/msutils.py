b63fb0b961cb318839e7dc44cd8681ae40e2aa37
Tammo Jan Dijkema <dijkema@astron.nl>
2019-01-23 15:32:52 +0100

    Copy flip_ra functions from apertif commissioning repo

--- apercal/subs/msutils.py
+++ apercal/subs/msutils.py
@@ -1,5 +1,7 @@
 import casacore.tables as pt
 import numpy as np
+from astropy.coordinates import Angle
+import astropy.units as u
 
 
 def has_good_modeldata(vis):
@@ -92,3 +94,52 @@ def get_nchan(msname):
     spectralwindowtable = pt.table(msname + '::SPECTRAL_WINDOW', ack=False)
     nchan = spectralwindowtable.getcol("CHAN_FREQ").shape[1]
     return nchan
+
+
+def format_dir(dir_rad):
+    """
+    Format an angle in ra, dec in sexagesimal format
+
+    Args:
+        dir_rad (Tuple[float, float]): Direction in ra, dec (in radians)
+
+    Returns:
+        str: formatted direction, e.g. 5h42m36.144s 49d51m07.2s
+    """
+    ra = Angle(dir_rad[0] * u.rad)
+    dec = Angle(dir_rad[1] * u.rad)
+    return (ra.to_string(u.hour) + " " + dec.to_string(u.degree)).encode('utf-8')
+
+
+def flip_ra(msname, logger=None):
+    """
+    Flip RA about central pointing, stored in REFERENCE_DIR
+
+    Args:
+        msname: full path to measurement set
+    """
+    t_field = pt.table(msname+"::FIELD", readonly=False)
+    phasedir = t_field[0]["PHASE_DIR"]
+    delaydir = t_field[0]["DELAY_DIR"]
+    refdir = t_field[0]["REFERENCE_DIR"]
+    newphasedir = np.copy(phasedir)
+
+    # Reflect delaydir around reference dir to get new phasedir
+    # This means that can run this code on a MS multiple times and it will work
+    newphasedir[0,0] = delaydir[0,0] + 2 * (refdir[0,0] - delaydir[0,0])
+
+    string_delay = format_dir(delaydir[0])
+    string_phase = format_dir(newphasedir[0])
+    log_msg = 'Changed delay direction from {} to {}'.format(string_delay, string_phase)
+    if logger:
+        logger.info(log_msg)
+    else:
+        print(log_msg)
+    pt.taql('INSERT INTO {}::HISTORY SET MESSAGE="{}", APPLICATION="apercal"'.format(msname, log_msg))
+
+    # Do the actual update
+    t_field.putcell("PHASE_DIR", 0, newphasedir)
+    t_field.flush()
+
+    # Recalculate uv coordinates
+    pt.taql('update {0} set UVW = mscal.newuvw()'.format(msname))
