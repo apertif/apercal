69aecbe775a86b6735e5acff6aa5ba648d6587b3
Tammo Jan Dijkema <dijkema@astron.nl>
2019-03-25 13:59:32 +0100

    Only do line for beams x0 and x7

--- apercal/pipeline/start_pipeline.py
+++ apercal/pipeline/start_pipeline.py
@@ -320,6 +320,8 @@ def start_apercal_pipeline(targets, fluxcals, polcals, dry_run=False, basedir=No
         logfilepath = os.path.join(basedir, 'apercal.log')
         lib.setup_logger('debug', logfile=logfilepath)
         for beamnr in beamlist_target:
+            if int(beamnr)%10 not in (0, 7):
+                continue
             try:
                 p6 = line(file_=configfilename)
                 p6.basedir = basedir
