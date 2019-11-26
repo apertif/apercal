65eed5f104d86103cb8eb7f87c13ba11eb75a1a1
Robert Schulz <schulz@astron.nl>
2019-11-26 08:41:12 +0100

    fixed function call

--- apercal/modules/mosaic_v2.py
+++ apercal/modules/mosaic_v2.py
@@ -514,7 +514,7 @@ class mosaic(BaseModule):
                 subs_managefiles.director(self, 'ch', self.mosaic_continuum_dir)
 
                 try:
-                    mosaic_utils.create_beams(self.mosaic_beam_list.copy(), self.mosaic_continuum_beam_subdir, corrtype=self.mosaic_primary_beam_type, primary_beam_path=self.mosaic_primary_beam_shape_files_location)
+                    mosaic_utils.create_beam(self.mosaic_beam_list.copy(), self.mosaic_continuum_beam_subdir, corrtype=self.mosaic_primary_beam_type, primary_beam_path=self.mosaic_primary_beam_shape_files_location)
                 except Exception as e:
                     error = "Creating map of beam {} ... Failed".format(beam)
                     logger.warning(error)
