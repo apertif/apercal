c8ca5bb61033972f9780827b2b02be31291b4edc
Robert Schulz <schulz@astron.nl>
2019-11-27 08:43:29 +0100

    debug message for math

--- apercal/modules/mosaic_v2.py
+++ apercal/modules/mosaic_v2.py
@@ -1267,7 +1267,7 @@ class mosaic(BaseModule):
                     if inv_cov[int(b),int(bm)]!=0.:
                         #operate+="<"+self.mosaic_continuum_beam_subdir+"/beam_{0}_mos.map>*{1}+".format(b,inv_cov[int(b),int(bm)])
                         operate="<"+self.mosaic_continuum_beam_subdir+"/beam_{0}_mos.map>*{1}".format(b,inv_cov[int(b),int(bm)])
-                    logger.debug("operate = {}".format(operate))
+                    logger.debug("for beam combination {0},{1}: operate = {2}".format(bm, b, operate))
                     maths.exp = operate
                     maths.options='unmask'
                     maths.inp()
