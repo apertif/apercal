9fc21b92cc0461d5e895363f8a43475a86f411c6
Robert Schulz <schulz@astron.nl>
2019-11-27 10:12:04 +0100

    changed beam map assignment in operate

--- apercal/modules/mosaic_v2.py
+++ apercal/modules/mosaic_v2.py
@@ -1263,10 +1263,11 @@ class mosaic(BaseModule):
             for b in self.mosaic_beam_list:
                 maths.out =os.path.join(self.mosaic_continuum_mosaic_subdir,'tmp_{}.map'.format(b))
                 # since the beam list is made of strings, need to convert to integers
-                if os.path.isdir(os.path.join(self.mosaic_continuum_beam_subdir, "beam_{0}_mos.map".format(b))):
+                beam_map = os.path.join(self.mosaic_continuum_beam_subdir, "beam_{0}_mos.map".format(b))
+                if os.path.isdir(beam_map):
                     if inv_cov[int(b),int(bm)]!=0.:
                         #operate+="<"+self.mosaic_continuum_beam_subdir+"/beam_{0}_mos.map>*{1}+".format(b,inv_cov[int(b),int(bm)])
-                        operate="<"+self.mosaic_continuum_beam_subdir+"/beam_{0}_mos.map>*{1}".format(b,inv_cov[int(b),int(bm)])
+                        operate+="<{0}>*{1}+".format(beam_map,inv_cov[int(b),int(bm)])
                     logger.debug("for beam combination {0},{1}: operate = {2}".format(bm, b, operate))
                     maths.exp = operate
                     maths.options='unmask'
