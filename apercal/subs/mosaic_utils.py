f533c272de0380df6ae080db7c00820d568a6aff
Robert Schulz <schulz@astron.nl>
2019-11-29 15:14:49 +0100

    converted tabs to spaces

--- apercal/subs/mosaic_utils.py
+++ apercal/subs/mosaic_utils.py
@@ -99,92 +99,93 @@ def make_gaussian_beam(beamdir, beamoutname, bm_size, cell, fwhm, cutoff):
 
 
 def get_measured_beam_maps(beam, beam_map_input_path, beam_map_output_path, beam_map_output_name):
-	"""
-	Function to create beam map from drift scans
+    """
+    Function to create beam map from drift scans
 
-	Args:
-		input:
-		beam = beam number (int, e.g. 01)
-		image_path = path to mir image
-		beam_map_path = path to the directory with measured beam maps (/data5/apertif/driftscans/fits_files/191023/)
-		output_name = output name of regridded beam map
-	"""
+    Args:
+        input:
+        beam = beam number (int, e.g. 01)
+        image_path = path to mir image
+        beam_map_path = path to the directory with measured beam maps (/data5/apertif/driftscans/fits_files/191023/)
+        output_name = output name of regridded beam map
+    """
 
-	work_dir = os.getcwd()
+    work_dir = os.getcwd()
 
-    temp_beam_model_name = os.path.join(beam_map_output_path, 'beam_model_{1}_temp.fits'.format(beam))
-    input_beam_model = os.path.join(beam_map_input_path, "{0}_{1}_I_mode.fits".format(os.path.basename(beam_map_input_path), beam))
+    temp_beam_model_name = os.path.join(
+        beam_map_output_path, 'beam_model_{1}_temp.fits'.format(beam))
+    input_beam_model = os.path.join(beam_map_input_path, "{0}_{1}_I_mode.fits".format(
+        os.path.basename(beam_map_input_path), beam))
 
-	# copy beam model to work directory
-    logger.debug("Copying beam model of beam {0} ({1} -> {2})".format(beam, input_beam_model,temp_beam_model_name))
+    # copy beam model to work directory
+    logger.debug("Copying beam model of beam {0} ({1} -> {2})".format(
+        beam, input_beam_model, temp_beam_model_name))
     # copy_cmd = 'cp -r {0} {1}'.format(input_beam_model, temp_beam_model_name)
     # copy_cmd = 'cp -r {0}191023_{1}_I_model.fits {2}'.format(
-	#     beam_map_input_path, beam, temp_beam_model_name)
+    #     beam_map_input_path, beam, temp_beam_model_name)
     # logger.debug(copy_cmd)
     # switched to python command
-    shutil.copy2(input_beam_model,temp_beam_model_name)
-	# os.system(copy_cmd)
+    shutil.copy2(input_beam_model, temp_beam_model_name)
+    # os.system(copy_cmd)
 
     logger.debug("Converting beam model of beam {}".format(beam))
-	# convert beam model to mir file
-	# if os.path.isdir('./beam_model_{0}_temp.mir'.format(beam)) == False:
+    # convert beam model to mir file
+    # if os.path.isdir('./beam_model_{0}_temp.mir'.format(beam)) == False:
     if os.path.isdir(os.path.join(beam_map_output_path, beam_map_output_name)) == False:
-		fits = lib.miriad('fits')
-		# fits.in_ = './beam_model_{0}_temp.fits'.format(beam)
-		fits.in_ = temp_beam_model_name
+        fits = lib.miriad('fits')
+        # fits.in_ = './beam_model_{0}_temp.fits'.format(beam)
+        fits.in_ = temp_beam_model_name
         fits.op = 'xyin'
         # fits.out = './beam_model_{0}_temp.mir'.format(beam)
-		fits.out = os.path.join(beam_map_output_path, beam_map_output_name)
-		fits.go()
-	
-	# if os.path.isdir('{}.mir'.format(image_path[:-5])) == False:
-	# 	fits = lib.miriad('fits')
-	# 	fits.in_ = image_path
-	# 	fits.op = 'xyin'
-	# 	fits.out = '{}.mir'.format(image_path[:-5])
-	# 	fits.go()
-	
-	
-	# replace centre of beam_model
-	
-	# gethd = lib.miriad('gethd')
-	# gethd.in_ = '{0}.mir/crval1'.format(image_path[:-5])
-	# ra_ref=gethd.go()
-	# gethd.in_ = '{0}.mir/crval2'.format(image_path[:-5])
-	# dec_ref=gethd.go()
-	
-	# print(ra_ref, dec_ref)
-	
-	# puthd = lib.miriad('puthd')
-	# puthd.in_ = './beam_model_{0}_temp.mir/crval1'.format(beam)
-	# puthd.value = ra_ref[0]
-	# puthd.type = 'double'
-	# puthd.go()
-	# puthd.in_ = './beam_model_{0}_temp.mir/crval2'.format(beam)
-	# puthd.value = dec_ref[0]
-	# puthd.type = 'double'
-	# puthd.go()
+        fits.out = os.path.join(beam_map_output_path, beam_map_output_name)
+        fits.go()
 
-	
-	# # regrid beam model
-	# if os.path.isdir('./beam_model_{0}.mir'.format(beam)) == False:
-	# 	regrid = lib.miriad('regrid')
-	# 	regrid.in_ = './beam_model_{0}_temp.mir'.format(beam)
-	# 	regrid.out = './{0}_{1}.mir'.format(output_name, beam)
-	# 	regrid.axes = '1,2'
-	# 	regrid.tin = '{0}.mir'.format(image_path[:-5])
-	# 	regrid.go()
-	
-	# print('DONE')
-	
-	# clean temp file 
-	
+    # if os.path.isdir('{}.mir'.format(image_path[:-5])) == False:
+    # 	fits = lib.miriad('fits')
+    # 	fits.in_ = image_path
+    # 	fits.op = 'xyin'
+    # 	fits.out = '{}.mir'.format(image_path[:-5])
+    # 	fits.go()
+
+    # replace centre of beam_model
+
+    # gethd = lib.miriad('gethd')
+    # gethd.in_ = '{0}.mir/crval1'.format(image_path[:-5])
+    # ra_ref=gethd.go()
+    # gethd.in_ = '{0}.mir/crval2'.format(image_path[:-5])
+    # dec_ref=gethd.go()
+
+    # print(ra_ref, dec_ref)
+
+    # puthd = lib.miriad('puthd')
+    # puthd.in_ = './beam_model_{0}_temp.mir/crval1'.format(beam)
+    # puthd.value = ra_ref[0]
+    # puthd.type = 'double'
+    # puthd.go()
+    # puthd.in_ = './beam_model_{0}_temp.mir/crval2'.format(beam)
+    # puthd.value = dec_ref[0]
+    # puthd.type = 'double'
+    # puthd.go()
+
+    # # regrid beam model
+    # if os.path.isdir('./beam_model_{0}.mir'.format(beam)) == False:
+    # 	regrid = lib.miriad('regrid')
+    # 	regrid.in_ = './beam_model_{0}_temp.mir'.format(beam)
+    # 	regrid.out = './{0}_{1}.mir'.format(output_name, beam)
+    # 	regrid.axes = '1,2'
+    # 	regrid.tin = '{0}.mir'.format(image_path[:-5])
+    # 	regrid.go()
+
+    # print('DONE')
+
+    # clean temp file
+
     logger.debug("Removing fits file of beam model of beam {}".format(beam))
     os.remove(temp_beam_model_name)
-	# os.system('rm -r ./*{}_temp.*'.format(beam))
+    # os.system('rm -r ./*{}_temp.*'.format(beam))
 
 
-def beam_cutoff(beamname,tmpbeamname,beamdir,cutoff):
+def beam_cutoff(beamname, tmpbeamname, beamdir, cutoff):
     """
     Function to apply a beam cutoff value
 
@@ -198,18 +199,18 @@ def beam_cutoff(beamname,tmpbeamname,beamdir,cutoff):
     maths = lib.miriad('maths')
     # expression is just beam map to have cutoff value applied
     # Use brackets to guard against formatting issues
-    maths.exp = "'<{0}/{1}>'".format(beamdir,tmpbeamname)
+    maths.exp = "'<{0}/{1}>'".format(beamdir, tmpbeamname)
     # Apply cutoff value as mask, using miriad formatting, brackets around image name
-    maths.mask = "'<{0}/{1}>.gt.{2}'".format(beamdir,tmpbeamname,cutoff)
-    maths.out = '{0}/{1}'.format(beamdir,beamname)
+    maths.mask = "'<{0}/{1}>.gt.{2}'".format(beamdir, tmpbeamname, cutoff)
+    maths.out = '{0}/{1}'.format(beamdir, beamname)
     # run
     maths.go()
 
     # clean up temp file
-    os.system('rm -rf {0}/{1}'.format(beamdir,tmpbeamname))
+    os.system('rm -rf {0}/{1}'.format(beamdir, tmpbeamname))
 
 
-def fixheader(beamname,beamdir):
+def fixheader(beamname, beamdir):
     """
     Generation of Gaussian beam maps results in the Gaussian
     parameters included as the "beam"
@@ -222,18 +223,19 @@ def fixheader(beamname,beamdir):
     # load function to delete header information
     delhd = lib.miriad('delhd')
     # delete bmaj
-    delhd.in_ = '{0}/{1}/bmaj'.format(beamdir,beamname)
+    delhd.in_ = '{0}/{1}/bmaj'.format(beamdir, beamname)
     delhd.go()
     # delete bmin
-    delhd.in_ = '{0}/{1}/bmin'.format(beamdir,beamname)
+    delhd.in_ = '{0}/{1}/bmin'.format(beamdir, beamname)
     delhd.go()
     # delete bmpa
-    delhd.in_ = '{0}/{1}/bpa'.format(beamdir,beamname)
+    delhd.in_ = '{0}/{1}/bpa'.format(beamdir, beamname)
     delhd.go()
-    
+
 # ++++++++++++++++++++++++++++++++++++++++
 # Functions to create a correlation matrix
 # ++++++++++++++++++++++++++++++++++++++++
+
 
 def correlation_matrix_symmetrize(a):
     """
@@ -242,110 +244,111 @@ def correlation_matrix_symmetrize(a):
 
     return a + a.T - np.diag(a.diagonal())
 
+
 def create_correlation_matrix(output_file):
     """
     This function creates a correlation matrix for 39 independent beams (i.e. an identity matrix)
     and writes it out to a file
     """
     # This needs to be multiplied by the variance for each beam still.
-    C=np.identity(40,dtype=np.float64)
+    C = np.identity(40, dtype=np.float64)
     # Fill array with estimated correlation coefficients (r in the nomenclature)
     # These are eyeballed based on ASKAP measurements.
-    C[0,17]=0.7
-    C[0,24]=0.7
-    C[0,18]=0.25
-    C[0,23]=0.25
-    C[1,2]=0.11
-    C[1,8]=0.11
-    C[2,3]=0.11
-    C[2,8]=0.11
-    C[2,9]=0.11
-    C[3,4]=0.11
-    C[3,9]=0.11
-    C[3,10]=0.11
-    C[4,5]=0.11
-    C[4,10]=0.11
-    C[4,11]=0.11
-    C[5,6]=0.11
-    C[5,11]=0.11
-    C[5,12]=0.11
-    C[6,7]=0.11
-    C[6,12]=0.11
-    C[6,13]=0.11
-    C[7,13]=0.11
-    C[7,14]=0.11
-    C[8,9]=0.11
-    C[8,15]=0.11
-    C[9,10]=0.11
-    C[9,15]=0.11
-    C[9,16]=0.11
-    C[10,11]=0.11
-    C[10,16]=0.11
-    C[10,17]=0.11
-    C[11,12]=0.11
-    C[11,17]=0.11
-    C[11,18]=0.11
-    C[12,13]=0.11
-    C[12,18]=0.11
-    C[12,19]=0.11
-    C[13,14]=0.11
-    C[13,19]=0.11
-    C[13,20]=0.11
-    C[14,20]=0.11
-    C[15,16]=0.11
-    C[15,21]=0.11
-    C[15,22]=0.11
-    C[16,17]=0.11
-    C[16,22]=0.11
-    C[16,23]=0.11
-    C[17,18]=0.11
-    C[17,23]=0.11
-    C[17,24]=0.11
-    C[18,19]=0.11
-    C[18,24]=0.11
-    C[18,25]=0.11
-    C[19,20]=0.11
-    C[19,25]=0.11
-    C[19,26]=0.11
-    C[20,26]=0.11
-    C[21,22]=0.11
-    C[21,27]=0.11
-    C[22,23]=0.11
-    C[22,27]=0.11
-    C[22,28]=0.11
-    C[23,24]=0.11
-    C[23,28]=0.11
-    C[23,29]=0.11
-    C[24,25]=0.11
-    C[24,29]=0.11
-    C[24,30]=0.11
-    C[25,26]=0.11
-    C[25,30]=0.11
-    C[25,31]=0.11
-    C[26,31]=0.11
-    C[26,32]=0.11
-    C[27,28]=0.11
-    C[27,33]=0.11
-    C[27,34]=0.11
-    C[28,29]=0.11
-    C[28,34]=0.11
-    C[28,35]=0.11
-    C[29,30]=0.11
-    C[29,35]=0.11
-    C[29,36]=0.11
-    C[30,31]=0.11
-    C[30,36]=0.11
-    C[30,37]=0.11
-    C[31,32]=0.11
-    C[31,37]=0.11
-    C[31,38]=0.11
-    C[32,38]=0.11
-    C[32,39]=0.11
-    C[33,34]=0.11
-    C[34,35]=0.11
-    C[35,36]=0.11
-    C[36,37]=0.11
-    C[37,38]=0.11
-    C[38,39]=0.11
+    C[0, 17] = 0.7
+    C[0, 24] = 0.7
+    C[0, 18] = 0.25
+    C[0, 23] = 0.25
+    C[1, 2] = 0.11
+    C[1, 8] = 0.11
+    C[2, 3] = 0.11
+    C[2, 8] = 0.11
+    C[2, 9] = 0.11
+    C[3, 4] = 0.11
+    C[3, 9] = 0.11
+    C[3, 10] = 0.11
+    C[4, 5] = 0.11
+    C[4, 10] = 0.11
+    C[4, 11] = 0.11
+    C[5, 6] = 0.11
+    C[5, 11] = 0.11
+    C[5, 12] = 0.11
+    C[6, 7] = 0.11
+    C[6, 12] = 0.11
+    C[6, 13] = 0.11
+    C[7, 13] = 0.11
+    C[7, 14] = 0.11
+    C[8, 9] = 0.11
+    C[8, 15] = 0.11
+    C[9, 10] = 0.11
+    C[9, 15] = 0.11
+    C[9, 16] = 0.11
+    C[10, 11] = 0.11
+    C[10, 16] = 0.11
+    C[10, 17] = 0.11
+    C[11, 12] = 0.11
+    C[11, 17] = 0.11
+    C[11, 18] = 0.11
+    C[12, 13] = 0.11
+    C[12, 18] = 0.11
+    C[12, 19] = 0.11
+    C[13, 14] = 0.11
+    C[13, 19] = 0.11
+    C[13, 20] = 0.11
+    C[14, 20] = 0.11
+    C[15, 16] = 0.11
+    C[15, 21] = 0.11
+    C[15, 22] = 0.11
+    C[16, 17] = 0.11
+    C[16, 22] = 0.11
+    C[16, 23] = 0.11
+    C[17, 18] = 0.11
+    C[17, 23] = 0.11
+    C[17, 24] = 0.11
+    C[18, 19] = 0.11
+    C[18, 24] = 0.11
+    C[18, 25] = 0.11
+    C[19, 20] = 0.11
+    C[19, 25] = 0.11
+    C[19, 26] = 0.11
+    C[20, 26] = 0.11
+    C[21, 22] = 0.11
+    C[21, 27] = 0.11
+    C[22, 23] = 0.11
+    C[22, 27] = 0.11
+    C[22, 28] = 0.11
+    C[23, 24] = 0.11
+    C[23, 28] = 0.11
+    C[23, 29] = 0.11
+    C[24, 25] = 0.11
+    C[24, 29] = 0.11
+    C[24, 30] = 0.11
+    C[25, 26] = 0.11
+    C[25, 30] = 0.11
+    C[25, 31] = 0.11
+    C[26, 31] = 0.11
+    C[26, 32] = 0.11
+    C[27, 28] = 0.11
+    C[27, 33] = 0.11
+    C[27, 34] = 0.11
+    C[28, 29] = 0.11
+    C[28, 34] = 0.11
+    C[28, 35] = 0.11
+    C[29, 30] = 0.11
+    C[29, 35] = 0.11
+    C[29, 36] = 0.11
+    C[30, 31] = 0.11
+    C[30, 36] = 0.11
+    C[30, 37] = 0.11
+    C[31, 32] = 0.11
+    C[31, 37] = 0.11
+    C[31, 38] = 0.11
+    C[32, 38] = 0.11
+    C[32, 39] = 0.11
+    C[33, 34] = 0.11
+    C[34, 35] = 0.11
+    C[35, 36] = 0.11
+    C[36, 37] = 0.11
+    C[37, 38] = 0.11
+    C[38, 39] = 0.11
     C = correlation_matrix_symmetrize(C)
-    np.savetxt(output_file,C,fmt='%f')
+    np.savetxt(output_file, C, fmt='%f')
