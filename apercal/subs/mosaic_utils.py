a97e3a4f0d1be2de82130924c63a55c2f5e28edc
Robert Schulz <schulz@astron.nl>
2019-11-29 14:57:43 +0100

    Adapted PB code to main mosaic code

--- apercal/subs/mosaic_utils.py
+++ apercal/subs/mosaic_utils.py
@@ -6,8 +6,8 @@ import os
 logger = logging.getLogger(__name__)
 
 """
-Module with functions to support the mosaic module. 
-Based on Beam_Functions.ipnyb by D.J. Pisano available 
+Module with functions to support the mosaic module.
+Based on Beam_Functions.ipnyb by D.J. Pisano available
 in the Apertif-comissioning repository
 """
 
@@ -15,12 +15,12 @@ in the Apertif-comissioning repository
 # ++++++++++++++++++++++++++++++++++++++++
 # Functions to create the beam maps
 # ++++++++++++++++++++++++++++++++++++++++
-def create_beam(beam, beam_map_dir, corrtype = 'Gaussian', primary_beam_path = None,
-                bm_size=3073,cell=4.0,fwhm=1950.0, cutoff=0.25):
+def create_beam(beam, beam_map_dir, corrtype='Gaussian', primary_beam_path=None,
+                bm_size=3073, cell=4.0, fwhm=1950.0, cutoff=0.25):
     """
     Function to create beam maps with miriad
-    
-    In contrast to the notebook, this function gets a 
+
+    In contrast to the notebook, this function gets a
     list of beams and creates the beam maps for these
     specific beams.
 
@@ -33,31 +33,34 @@ def create_beam(beam, beam_map_dir, corrtype = 'Gaussian', primary_beam_path = N
         fwhm (float): FWHM in arcsec for type='Gaussian' (default 32.5*60)
         cutoff (float): Relative power level to cut beam off at
     """
-    #iterate through beams:
-    #for beam,beamdir in zip(beam_list,beam_map_dir):
-        
-    #first define output name (goes in outdir)
-    #use beam integer in name
+    # iterate through beams:
+    # for beam,beamdir in zip(beam_list,beam_map_dir):
+
+    # first define output name (goes in outdir)
+    # use beam integer in name
     beamoutname = 'beam_{}.map'.format(beam.zfill(2))
 
     # check if file exists:
     if not os.path.isdir(beamoutname):
-        #then test type and proceed for different types
+        # then test type and proceed for different types
         if corrtype == 'Gaussian':
-            make_gaussian_beam(beam_map_dir,beamoutname,bm_size,cell,fwhm,cutoff)
+            make_gaussian_beam(beam_map_dir, beamoutname,
+                               bm_size, cell, fwhm, cutoff)
         elif corrtype == 'Correct':
-            error='Measured PB maps not yet supported'
+            error = 'Measured PB maps not yet supported'
             logger.error(error)
             raise ApercalException(error)
             get_measured_beam_maps(beam, beam_map_dir, primary_beam_path)
         else:
-            error='Type of beam map not supported'
+            error = 'Type of beam map not supported'
             logger.error(error)
             raise ApercalException(error)
     else:
-        logger.warning("Beam map for beam {} already exists. Did not create it again".format(beam))
+        logger.warning(
+            "Beam map for beam {} already exists. Did not create it again".format(beam))
 
-def make_gaussian_beam(beamdir,beamoutname,bm_size,cell,fwhm,cutoff):
+
+def make_gaussian_beam(beamdir, beamoutname, bm_size, cell, fwhm, cutoff):
     """
     Function to create Gaussian beam
 
@@ -69,99 +72,113 @@ def make_gaussian_beam(beamdir,beamoutname,bm_size,cell,fwhm,cutoff):
         fwhm (float): FWHM of Gaussian in arcsec
         cutoff (float): Relative level to cut beam off at
     """
-    #use a temp beam name because you have to apply a cutoff and then clean up
+    # use a temp beam name because you have to apply a cutoff and then clean up
     tmpbeamname = beamoutname+'_tmp'
-    #set peak level to 1 and PA = 0, plus arbitrary reference pixel
+    # set peak level to 1 and PA = 0, plus arbitrary reference pixel
     pk = 1.
     bpa = 0.
     pos = [0., 60.]
-    #set up imgen parameters
+    # set up imgen parameters
     imgen = lib.miriad('imgen')
     imgen.out = os.path.join(beamdir, tmpbeamname)
     imgen.imsize = bm_size
     imgen.cell = cell
     imgen.object = 'gaussian'
-    imgen.spar = '{0},{1},{2},{3},{4},{5}'.format(str(pk),str(0),str(0),
-                                                  str(fwhm),str(fwhm),str(bpa))
-    imgen.radec = '{0},{1}'.format(str(pos[0]),str(pos[1]))
-    #create image
+    imgen.spar = '{0},{1},{2},{3},{4},{5}'.format(str(pk), str(0), str(0),
+                                                  str(fwhm), str(fwhm), str(bpa))
+    imgen.radec = '{0},{1}'.format(str(pos[0]), str(pos[1]))
+    # create image
     imgen.go()
 
-    #apply beam cutoff
-    beam_cutoff(beamoutname,tmpbeamname,beamdir,cutoff)
+    # apply beam cutoff
+    beam_cutoff(beamoutname, tmpbeamname, beamdir, cutoff)
 
-    #fix header
-    fixheader(beamoutname,beamdir)
+    # fix header
+    fixheader(beamoutname, beamdir)
 
-def get_measured_beam_maps(beam, image_path, beam_map_path, output_name):
+# def get_measured_beam_maps(beam, image_path, beam_map_path, output_name):
+
+
+def get_measured_beam_maps(beam, beam_map_input_path, beam_map_output_path, beam_map_output_name):
 	"""
 	Function to create beam map from drift scans
 
 	Args:
-		input: 
+		input:
 		beam = beam number (int, e.g. 01)
 		image_path = path to mir image
 		beam_map_path = path to the directory with measured beam maps (/data5/apertif/driftscans/fits_files/191023/)
 		output_name = output name of regridded beam map
 	"""
-	
+
 	work_dir = os.getcwd()
-	
+
+    temp_beam_model_name = os.path.join(beam_map_output_path, 'beam_model_{1}_temp.fits'.format(beam))
+
 	# copy beam model to work directory
-	os.system('cp -r {0}191023_{1}_I_model.fits {2}/beam_model_{1}_temp.fits'.format(beam_map_path, beam, work_dir)) 
-	
-	
+    logger.debug("Copying beam model of beam {}".format(beam))
+    copy_cmd = 'cp -r {0}191023_{1}_I_model.fits {2}'.format(
+	    beam_map_input_path, beam, temp_beam_model_name)
+    logger.debug(copy_cmd)
+	os.system(copy_cmd)
+
+    logger.debug("Converting beam model of beam {}".format(beam))
 	# convert beam model to mir file
-	if os.path.isdir('./beam_model_{0}_temp.mir'.format(beam)) == False:
+	# if os.path.isdir('./beam_model_{0}_temp.mir'.format(beam)) == False:
+    if os.path.isdir(os.path.join(beam_map_output_path, beam_map_output_name)) == False:
 		fits = lib.miriad('fits')
-		fits.in_ = './beam_model_{0}_temp.fits'.format(beam)
-		fits.op = 'xyin'
-		fits.out = './beam_model_{0}_temp.mir'.format(beam)
+		# fits.in_ = './beam_model_{0}_temp.fits'.format(beam)
+		fits.in_ = temp_beam_model_name
+        fits.op = 'xyin'
+        # fits.out = './beam_model_{0}_temp.mir'.format(beam)
+		fits.out = os.path.join(beam_map_output_path, beam_map_output_name)
 		fits.go()
 	
-	if os.path.isdir('{}.mir'.format(image_path[:-5])) == False:
-		fits = lib.miriad('fits')
-		fits.in_ = image_path
-		fits.op = 'xyin'
-		fits.out = '{}.mir'.format(image_path[:-5])
-		fits.go()
+	# if os.path.isdir('{}.mir'.format(image_path[:-5])) == False:
+	# 	fits = lib.miriad('fits')
+	# 	fits.in_ = image_path
+	# 	fits.op = 'xyin'
+	# 	fits.out = '{}.mir'.format(image_path[:-5])
+	# 	fits.go()
 	
 	
 	# replace centre of beam_model
 	
-	gethd = lib.miriad('gethd')
-	gethd.in_ = '{0}.mir/crval1'.format(image_path[:-5])
-	ra_ref=gethd.go()
-	gethd.in_ = '{0}.mir/crval2'.format(image_path[:-5])
-	dec_ref=gethd.go()
+	# gethd = lib.miriad('gethd')
+	# gethd.in_ = '{0}.mir/crval1'.format(image_path[:-5])
+	# ra_ref=gethd.go()
+	# gethd.in_ = '{0}.mir/crval2'.format(image_path[:-5])
+	# dec_ref=gethd.go()
 	
-	print(ra_ref, dec_ref)
+	# print(ra_ref, dec_ref)
 	
-	puthd = lib.miriad('puthd')
-	puthd.in_ = './beam_model_{0}_temp.mir/crval1'.format(beam)
-	puthd.value = ra_ref[0]
-	puthd.type = 'double'
-	puthd.go()
-	puthd.in_ = './beam_model_{0}_temp.mir/crval2'.format(beam)
-	puthd.value = dec_ref[0]
-	puthd.type = 'double'
-	puthd.go()
+	# puthd = lib.miriad('puthd')
+	# puthd.in_ = './beam_model_{0}_temp.mir/crval1'.format(beam)
+	# puthd.value = ra_ref[0]
+	# puthd.type = 'double'
+	# puthd.go()
+	# puthd.in_ = './beam_model_{0}_temp.mir/crval2'.format(beam)
+	# puthd.value = dec_ref[0]
+	# puthd.type = 'double'
+	# puthd.go()
 
 	
-	# regrid beam model
-	if os.path.isdir('./beam_model_{0}.mir'.format(beam)) == False:
-		regrid = lib.miriad('regrid')
-		regrid.in_ = './beam_model_{0}_temp.mir'.format(beam)
-		regrid.out = './{0}_{1}.mir'.format(output_name, beam)
-		regrid.axes = '1,2'
-		regrid.tin = '{0}.mir'.format(image_path[:-5])
-		regrid.go()
+	# # regrid beam model
+	# if os.path.isdir('./beam_model_{0}.mir'.format(beam)) == False:
+	# 	regrid = lib.miriad('regrid')
+	# 	regrid.in_ = './beam_model_{0}_temp.mir'.format(beam)
+	# 	regrid.out = './{0}_{1}.mir'.format(output_name, beam)
+	# 	regrid.axes = '1,2'
+	# 	regrid.tin = '{0}.mir'.format(image_path[:-5])
+	# 	regrid.go()
 	
-	print('DONE')
+	# print('DONE')
 	
 	# clean temp file 
 	
-	os.system('rm -r ./*{}_temp.*'.format(beam))
+    logger.debug("Removing fits file of beam model of beam {}".format(beam))
+    os.remove(temp_beam_model_name)
+	# os.system('rm -r ./*{}_temp.*'.format(beam))
 
 
 def beam_cutoff(beamname,tmpbeamname,beamdir,cutoff):
@@ -174,18 +191,18 @@ def beam_cutoff(beamname,tmpbeamname,beamdir,cutoff):
         beamdir (str): output directory for beam map
         cutoff (float): relative level at which beam map should be cut off
     """
-    #load maths
+    # load maths
     maths = lib.miriad('maths')
-    #expression is just beam map to have cutoff value applied
-    #Use brackets to guard against formatting issues
+    # expression is just beam map to have cutoff value applied
+    # Use brackets to guard against formatting issues
     maths.exp = "'<{0}/{1}>'".format(beamdir,tmpbeamname)
-    #Apply cutoff value as mask, using miriad formatting, brackets around image name
+    # Apply cutoff value as mask, using miriad formatting, brackets around image name
     maths.mask = "'<{0}/{1}>.gt.{2}'".format(beamdir,tmpbeamname,cutoff)
     maths.out = '{0}/{1}'.format(beamdir,beamname)
-    #run
+    # run
     maths.go()
 
-    #clean up temp file
+    # clean up temp file
     os.system('rm -rf {0}/{1}'.format(beamdir,tmpbeamname))
 
 
@@ -199,15 +216,15 @@ def fixheader(beamname,beamdir):
         beamname (str): name of beam map
         beamdir (str): location of beam map
     """
-    #load function to delete header information
+    # load function to delete header information
     delhd = lib.miriad('delhd')
-    #delete bmaj
+    # delete bmaj
     delhd.in_ = '{0}/{1}/bmaj'.format(beamdir,beamname)
     delhd.go()
-    #delete bmin
+    # delete bmin
     delhd.in_ = '{0}/{1}/bmin'.format(beamdir,beamname)
     delhd.go()
-    #delete bmpa
+    # delete bmpa
     delhd.in_ = '{0}/{1}/bpa'.format(beamdir,beamname)
     delhd.go()
     
