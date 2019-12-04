55d682d16e9c3ee52afd69af0efdb00fb961b794
Robert Schulz <schulz@astron.nl>
2019-12-04 18:53:28 +0100

    added some logging messages

--- apercal/modules/ccal.py
+++ apercal/modules/ccal.py
@@ -1403,6 +1403,8 @@ class ccal(BaseModule):
         
         n_stokes = pol_array.shape[2]  # shape is time, one, nstokes
     
+        logger.info("Beam {}: Inspecting autocorrelation of each antenna".format(self.beam))
+
         # list of flags
         flag_ant = []
         flag_pol = []
@@ -1411,10 +1413,11 @@ class ccal(BaseModule):
         pol_list_ms = [0, 3]
         pol_list = ['XX', 'YY']
 
-        #take MS file and get calibrated data
+        # take MS file and get calibrated data
         # amp_ant_array = np.empty(
         #     (len(ant_names), len(freqs), n_stokes), dtype=np.float64)        
         for ant, ant_name in enumerate(ant_names):
+            # getting the autocorrelation amplitude
             try:
                 taql_command = ("SELECT abs(gmeans(CORRECTED_DATA[FLAG])) AS amp "
                                 "FROM {0} "
@@ -1425,7 +1428,7 @@ class ccal(BaseModule):
             except Exception as e:
                 logger.warning("Beam {}: Could not get autocorrelation information for antenna {}".format(self.beam, ant_name))
                 logger.exception(e)
-                continue
+                continue    
             
             # get XX and YY
             # amp_XX = amp_ant[:, 0]
@@ -1441,15 +1444,20 @@ class ccal(BaseModule):
                 freqs_selected = freqs[np.where(amp != 0)[0]]
                 amp_selected = amp[np.where(amp != 0)[0]]
 
+                if len(amp_selected) == 0:
+                    logger.warning("Beam {0}, Antenna {1} (polarization {2}): No autocorrelation data available".format(
+                        self.beam, ant_name, pol_list[pol_nr], ratio_vis_above_limit))
+                    continue
+
                 ratio_vis_above_limit = ccal_utils.get_ratio_autocorrelation_above_limit(amp_selected, self.crosscal_autocorrelation_amp_limit)
                 if ratio_vis_above_limit > self.crosscal_autocorrelation_data_fraction_limit:
                     logger.info(
-                        "Beam {0}, Antenna {1} ({2}): fraction of autocorrelation data above amplitude threshold: {3} => Flagging polarisation {2}".format(self.beam, ant_name, pol_list[pol_nr], ratio_vis_above_limit))
+                        "Beam {0}, Antenna {1} (polarization {2}): fraction of autocorrelation data above amplitude threshold: {3} => Flagging polarisation {2}".format(self.beam, ant_name, pol_list[pol_nr], ratio_vis_above_limit))
                     flag_ant = flag_ant.append(ant_name)
                     flag_pol = flag_pol.append(pol_list[pol_nr])
                 else:
                     logger.info(
-                        "Beam {0}, Antenna {1} ({2}): fraction of autocorrelation data above amplitude threshold: {3} => No flagging".format(self.beam, ant_name, pol_list[pol_nr], ratio_vis_above_limit_xx))
+                        "Beam {0}, Antenna {1} (polarization {2}): fraction of autocorrelation data above amplitude threshold: {3} => No flagging".format(self.beam, ant_name, pol_list[pol_nr], ratio_vis_above_limit_xx))
                 
             # run check of fit to autocorrelation
             logger.info("Checking fit of autocorrelation not yet available")
@@ -1457,7 +1465,7 @@ class ccal(BaseModule):
             # run check of bandpass phase solutions
             logger.info("Checking bandpass phase solution not yet available")
             
-        if len(flag_nat) != 0:
+        if len(flag_ant) != 0:
             logger.info("Flaggging data")
             
             for ant, pol in zip(flag_ant, flag_pol):
